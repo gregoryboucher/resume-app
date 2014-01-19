49cff69d7273e090e91af927902835971371c988
Gregory <me@gregoryboucher.com>
2014-01-18 22:19:03 -0700

    moved test to match app dir structure

---
+++ src/test/scala/app/resumeAppServletSpec.scala
@@ -0,0 +1,16 @@
+package app
+
+import org.scalatra.test.specs2._
+
+// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
+class resumeAppServletSpec extends ScalatraSpec { def is =
+  "GET / on resumeAppServlet"                     ^
+    "should return status 200"                  ! root200^
+                                                end
+
+  addServlet(classOf[resumeAppServlet], "/*")
+
+  def root200 = get("/") {
+    status must_== 200
+  }
+}
